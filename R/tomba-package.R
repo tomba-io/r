5c33f5932f36754b969824c8bcafe555f6d6b608
Mohamed Ben <b.mohamed@tomba.io>
2021-10-28 12:42:48 +0100

    chore: add title and description

--- R/tomba-package.R
+++ R/tomba-package.R
@@ -1,5 +1,5 @@
-#' The official R client library for the tomba.io
-#'
+#' @title The official R client library for the tomba.io
+#' @description Search emails are based on the website You give one domain name and it returns all the email addresses found on the internet. at <https://tomba.io/>
 #'
 #' @md
 #' @name tomba
